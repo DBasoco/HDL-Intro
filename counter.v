eb073a514353625f97ffaa1f21a9f746d60fe053
DBasoco <65093434+DBasoco@users.noreply.github.com>
2020-06-16 14:56:30 -0600

    Add files via upload

---
+++ counter.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:51:21 06/16/2020 
+// Design Name: 
+// Module Name:    counter 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module counter(
+    );
+
+
+endmodule
